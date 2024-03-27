3b538bdc54cebfd6adc35eb52d71331b52e4a830
Carl Qi <daguqihanwen@berkeley.edu>
2024-03-26 20:05:41 -0500

    ppo runs

--- autonomous/buffer.py
+++ autonomous/buffer.py
@@ -32,7 +32,7 @@ class BCBuffer:
             )
     
 class PPOBuffer:
-    def __init__(self, obs_dim, act_dim, size, gamma=0.99, lam=0.95):
+    def __init__(self, obs_dim, act_dim, size, device, gamma=0.99, lam=0.95):
         self.obs_buf = np.zeros((size, obs_dim), dtype=np.float32)
         self.act_buf = np.zeros((size, act_dim), dtype=np.float32)
         self.adv_buf = np.zeros(size, dtype=np.float32)
@@ -43,6 +43,7 @@ class PPOBuffer:
         self.gamma, self.lam = gamma, lam
         self.ptr, self.path_start_idx, self.max_size = 0, 0, size
         self.mean_ret = 0
+        self.device = device
 
     def store(self, obs, act, rew, val, logp):
         assert self.ptr < self.max_size
@@ -51,6 +52,8 @@ class PPOBuffer:
         self.rew_buf[self.ptr] = rew
         self.val_buf[self.ptr] = val
         self.logp_buf[self.ptr] = logp
+
+        self.ptr = (self.ptr + 1) % self.max_size
 
     def reset(self):
         self.ptr, self.path_start_idx = 0, 0
@@ -74,8 +77,8 @@ class PPOBuffer:
 
     def sample_batch(self, batch_size=32):
         idxs = np.random.randint(0, self.ptr, size=batch_size)
-        return dict(obs=torch.as_tensor(self.obs_buf[idxs], dtype=torch.float32),
-                    act=torch.as_tensor(self.act_buf[idxs], dtype=torch.float32),
-                    adv=torch.as_tensor(self.adv_buf[idxs], dtype=torch.float32),
-                    ret=torch.as_tensor(self.ret_buf[idxs], dtype=torch.float32),
-                    logp=torch.as_tensor(self.logp_buf[idxs], dtype=torch.float32))
+        return dict(obs=torch.as_tensor(self.obs_buf[idxs], dtype=torch.float32).to(self.device),
+                    act=torch.as_tensor(self.act_buf[idxs], dtype=torch.float32).to(self.device),
+                    adv=torch.as_tensor(self.adv_buf[idxs], dtype=torch.float32).to(self.device),
+                    ret=torch.as_tensor(self.ret_buf[idxs], dtype=torch.float32).to(self.device),
+                    logp=torch.as_tensor(self.logp_buf[idxs], dtype=torch.float32).to(self.device))
